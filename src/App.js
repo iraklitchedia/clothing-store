08dd2585191b1868f2d975d7aca556492be2a247
Irakli Tchedia <iraklitchedia@gmail.com>
2021-06-19 12:38:18 +0400

    Added redux and applied it to saving current user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.scss';
 import ShopPage from './pages/shop/shop.component';
@@ -7,16 +8,9 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			currentUser: null
-		}
-	}
-
 	unsubscribeFromAuth = null;
 	unsubscribeFromSnapshot = null;
 
@@ -24,14 +18,12 @@ class App extends React.Component {
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 			const userRef = await createUserProfileDocument(userAuth);
 			if(!userRef) {
-				this.setState( {
-					currentUser: null
-				});
+				this.props.setCurrentUser(null);
 				return;
 			}
 
 			this.unsubscribeFromSnapshot = userRef.onSnapshot(snapshot => {
-				this.setState({ currentUser: {
+				this.props.setCurrentUser({ currentUser: {
 					id: snapshot.id,
 					...snapshot.data()
 				}});
@@ -49,7 +41,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-				<Header currentUser={this.state.currentUser} />
+				<Header />
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route exact path='/shop' component={ShopPage} />
@@ -60,4 +52,8 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
